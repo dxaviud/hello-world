24b026c69d31cad4b599f5dd358f0c2bf32e3490
David Xu <62825020+d-1-10110-1001-d@users.noreply.github.com>
2020-06-08 17:20:36 -0400

    added alternate function for printing fizzbuzz.

--- fizz_buzz.py
+++ fizz_buzz.py
@@ -14,6 +14,19 @@ def print_fizz_buzz(until):
 
 		print(output)
 
+def print_fizz_buzz_v2(until):
+	for number in range(1,until+1):
+		number_is_divisible = False
+		if (first_mod_second_is_third(number, 3, 0)):
+			print(fizz, end = "")
+			number_is_divisible = True
+		if (first_mod_second_is_third(number, 5, 0)):
+			print(buzz, end = "")
+			number_is_divisible = True
+		if (number_is_divisible == False):
+			print(number, end = "")
+		print()
+
 #first, second, and third must all be integers
 def first_mod_second_is_third(first, second, third):
 	if (first % second == third):
@@ -21,3 +34,7 @@ def first_mod_second_is_third(first, second, third):
 	return False
 
 print_fizz_buzz(30)
+
+print()
+
+print_fizz_buzz_v2(30)

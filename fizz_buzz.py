ce990d0de527752c889fcce308dcf3186d52b84c
David Xu <62825020+d-1-10110-1001-d@users.noreply.github.com>
2020-06-07 22:00:21 -0400

    improved efficiency

--- fizz_buzz.py
+++ fizz_buzz.py
@@ -4,14 +4,20 @@ buzz = "buzz"
 #until should be > 1
 def print_fizz_buzz(until):
 	for number in range(1,until+1):
-		if number%3 == 0:
-			if number%5 == 0:
-				print(fizz+buzz)
-			else:
-				print(fizz)
-		elif number%5 == 0:
-			print(buzz)
-		else:
-			print(number)
+		output = ""
+		if(first_mod_second_is_third(number, 3, 0)):
+			output += fizz
+		if(first_mod_second_is_third(number, 5, 0)):
+			output += buzz
+		if (output == ""):
+			output = number
+
+		print(output)
+
+#first, second, and third must all be integers
+def first_mod_second_is_third(first, second, third):
+	if (first % second == third):
+		return True
+	return False
 
 print_fizz_buzz(30)

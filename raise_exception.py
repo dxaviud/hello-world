cf5fa971162b3f1c50a7f0ce4687b5dc74bb190e
David Xu <62825020+d-1-10110-1001-d@users.noreply.github.com>
2020-06-08 23:03:56 -0400

    Added comments

--- raise_exception.py
+++ raise_exception.py
@@ -1,5 +1,10 @@
+#Made this to practice raising exceptions
 
 def number_contained_inclusive(number, min_value, max_value):
+	'''
+	returns True if number is between min_value and max_value, inclusive. Returns False if this is not the case. 
+	raises an exception if min_value is greater than max_value.
+	'''
 	if (min_value > max_value):
 		raise Exception("Error: lower bound is greater than upper bound.")
 	try:
@@ -11,5 +16,9 @@ def number_contained_inclusive(number, min_value, max_value):
 	except ValueError:
 		print("Must have an int argument. Function terminated.")
 
+one_contained_inclusive_zero_ten = number_contained_inclusive(1, 0, 10)
+print(one_contained_inclusive_zero_ten)
+one_contained_inclusive_zero_ten = number_contained_inclusive("a string", 0, 10)
+print(one_contained_inclusive_zero_ten)
 one_contained_inclusive_zero_ten = number_contained_inclusive(1, 10, 0)
 print(one_contained_inclusive_zero_ten)

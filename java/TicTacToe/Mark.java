3f858fb917845faa913136932cab9948921303a8
David Xu <62825020+d-1-10110-1001-d@users.noreply.github.com>
2020-06-10 18:50:36 -0400

    overrode the equals method

--- java/TicTacToe/Mark.java
+++ java/TicTacToe/Mark.java
@@ -12,6 +12,14 @@ public abstract class Mark {
     public void setImage(Image newImage) {
         image = newImage;
     }
-    
+
+    public boolean equals(Object otherMark) {
+        otherMark = (Mark) otherMark;
+        if (otherMark.toString().equals(this.toString())) {
+            return true;
+        }
+        return false;
+    }
+
     public abstract String toString();
 }

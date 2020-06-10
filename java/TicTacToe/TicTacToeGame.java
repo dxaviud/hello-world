7d06632b8417d8eb89ecaf013727d134c86ae83c
David Xu <62825020+d-1-10110-1001-d@users.noreply.github.com>
2020-06-10 17:29:29 -0400

    created TicTacToeGame.java

---
+++ java/TicTacToe/TicTacToeGame.java
@@ -0,0 +1,25 @@
+package TicTacToe;
+
+public class TicTacToeGame {
+    private TicTacToeBoard gameBoard;
+
+    public TicTacToeGame() {
+        gameBoard = new TicTacToeBoard();
+    }
+
+    public void runGame() {
+
+    }
+
+    public void xMove() {
+
+    }
+
+    public void oMove() {
+
+    }
+
+    public boolean getGameOver() {
+
+    }
+}

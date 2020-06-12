c24a3c6ba53045dfc755ea481cf502a9ff4fca2a
David Xu <62825020+d-1-10110-1001-d@users.noreply.github.com>
2020-06-12 00:15:51 -0400

    added static method to Book class and class variable to Contact class

--- python/using_classes.py
+++ python/using_classes.py
@@ -1,13 +1,21 @@
 #Made this program to practice with creating classes and class instances
 
 class Contact:
-    
+
+    #This is a class variable
+    phone_number_length = 10
+
     def __init__(self, name="Nameless", phone_number="0000000000", address="Nowhere"):
         self.name = name
         self.phone_number = phone_number
         self.address = address
 
 class Book:
+
+    @staticmethod
+    def a_static_method():
+        print("This is a static method.")
+
 
     def __init__(self, title="Untitled", author="Nobody", publish_year="Never published"):
         self.title = title
@@ -26,4 +34,9 @@ print(my_book.title, my_book.author, my_book.publish_year)
 
 my_favorite_book = Book(title = "Favorite book", author = "Me", publish_year = "0")
 print(my_favorite_book.title, my_favorite_book.author, my_favorite_book.publish_year)
+
+print("The Contact class has a class variable called phone_number_length:", Contact.phone_number_length)
+
+print("The Book class has a static method: ")
+Book.a_static_method()
 

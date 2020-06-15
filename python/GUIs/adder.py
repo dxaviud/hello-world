c809e0b6c4ae96420b59dbcc975b466c40b5a381
David Xu <62825020+d-1-10110-1001-d@users.noreply.github.com>
2020-06-15 18:21:03 -0400

    relocated function

--- python/GUIs/adder.py
+++ python/GUIs/adder.py
@@ -7,6 +7,13 @@ class Adder:
     def display(self):
         root = Tk(screenName="adder")
 
+        def turn_entry_red(entry):
+            entry.config(background="red")
+
+        def turn_entry_normal(entry):
+            entry.config(background="white")
+
+
         first_number_label = Label(root, text = "First Number")
         first_number_label.grid(sticky=E, padx=5, pady=5, row=0, column=0)
 
@@ -22,17 +29,32 @@ class Adder:
         result_label = Label(root, text="Result")
         result_label.grid(sticky=E+W, padx=5, pady=5, columnspan=2, row=3, column=0)
         
+
+
         def add_event():
 
+            error_message = ""
             first_number_text = first_number_entry.get()
             second_number_text = second_number_entry.get()
             try:
                 first_number = float(first_number_text)
-                second_number = float(second_number_text)
+                turn_entry_normal(first_number_entry)
             except ValueError:
-                print("One or both entry incorrect.")
-                return    
-            result = first_number+second_number
+                turn_entry_red(first_number_entry)
+                error_message += "First number invalid.\n"   
+
+            try:
+                second_number = float(second_number_text)
+                turn_entry_normal(second_number_entry)
+            except ValueError:
+                turn_entry_red(second_number_entry)
+                error_message += "Second number invalid."
+            
+            if error_message != "":
+                result = error_message
+            else:
+                result = first_number+second_number
+                
             result_label.config(text=str(result))
 
         add_button = Button(root, text="Add", command=add_event)

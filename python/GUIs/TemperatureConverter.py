b0da11fcf59fb8477c68b2e933cacedba7498d44
David Xu <62825020+d-1-10110-1001-d@users.noreply.github.com>
2020-06-15 18:30:14 -0400

    Handled ValueError exception and finished program

--- python/GUIs/TemperatureConverter.py
+++ python/GUIs/TemperatureConverter.py
@@ -25,6 +25,9 @@ class Converter(object):
         cent_entry = Entry(root, width=5)
         cent_entry.grid(row=0, column=1, padx=5, pady=5)
 
+        fah_label = Label(root, text="Fahrenheit:")
+        fah_label.grid(row=2, column=0, padx=5, pady=5)
+
         fah_entry = Entry(root, width=5)
         fah_entry.grid(row=2, column=1, padx=5, pady=5)
 
@@ -33,7 +36,13 @@ class Converter(object):
             Convert from fahrenheit to centigrade and display the result
             '''
             fah_string = fah_entry.get()
-            fah_float = float(fah_string)
+
+            try:
+                fah_float = float(fah_string)
+            except ValueError:
+                print("Fahrenheit temperature is invalid")
+                return
+
             result = (fah_float - 32) / 1.89
             cent_entry.delete(0) # remove the old text
             cent_entry.insert(0, str(result)) # insert the new text
@@ -43,11 +52,24 @@ class Converter(object):
             Convert from centigrade to fahrenheit and display the result
             '''
             cent_string = cent_entry.get()
-            cent_float = float(cent_string)
+
+            try:
+                cent_float = float(cent_string)
+            except ValueError:
+                print("Celsius temperature is invalid")
+                return
+
+                
             result = cent_float * 1.8 + 32
+            fah_entry.delete(0) # remove the old text
+            fah_entry.insert(0, str(result)) # insert the new text
+
 
         fah_to_cent_button = Button(root, text="Fah to cent", command=fah_to_cent)
         fah_to_cent_button.grid(row=1, column=0, padx=5, pady=5)
+
+        cent_to_fah_button = Button(root, text="Cent to fah", command=cent_to_fah)
+        cent_to_fah_button.grid(row=1, column=1, padx=5, pady=5)
 
         root.mainloop()
 
